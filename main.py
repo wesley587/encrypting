479f9681aff022e5167258ceff19ae1f71232a94
Wesley Cesar <72465364+wesley587@users.noreply.github.com>
2021-05-31 12:37:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,16 +17,8 @@ arguments.add_argument('-i --interactive', dest='interactive', default=False, co
 arguments.add_argument('-s --save', dest='save', default=False, help='Save the stdout in a file', const='', nargs='?')
 arguments.add_argument('-p --path', dest='path', help='Inform the path to save the file')
 arguments.add_argument('-nk --numkeys', dest='numkeys', help='Show all keys on keys folder', const='', nargs='?', default=False)
-arguments.add_argument('-g -generatekey', dest='new_key', nargs='?', const='', help='Generate new key value')
-
-try:
-    os.mkdir('decrypt')
-except:
-    pass
-try:
-    os.mkdir('keys')
-except:
-    pass
+arguments.add_argument('-g --generatekey', dest='new_key', nargs='?', const='', help='Generate new key value')
+arguments.add_argument('-k --key', default='secret.key', dest='key', help='Ke that project will use..')
 
 class crypt_end_decrypt:
     def __init__(self):
@@ -35,9 +27,12 @@ class crypt_end_decrypt:
         if parse.interactive or parse.interactive == '' or parse.interactive == ' ':
             action = str(input('Action read or write? [w/r/nk/g] ')).lower().strip()
             if action == 'r' or action == 'read':
+                keys = self.infokes(storage=True)
+                key = str(input('Key num:'))
+                self.num_key = {'default': v for k, v in keys.items() if k == key}['default'] 
                 self.write = False
                 self.read = str(input('Path: '))
-                self.save = True if str(input('Save the output? [y/n]')) == 'y' else False
+                self.save = True if str(input('Save the output? [y/n]')) == 'y'  else False
                 if self.save:
                     path = str(input('Path to save: [path/default]'))
                     if path.lower().strip() == 'default':    
@@ -45,6 +40,9 @@ class crypt_end_decrypt:
                     else:
                         self.path = path
             elif action == 'w' or action == 'write':
+                keys = self.infokes(storage=True)
+                key = str(input('Key num:'))
+                self.num_key = {'default': v for k, v in keys.items() if k == key}['default'] 
                 self.reading = False
                 self.write = str(input('Message: '))
                 validation = str(input('save in a expecify path or not? [y/n]')).lower().strip()[0]
@@ -72,6 +70,9 @@ class crypt_end_decrypt:
                 elif parse.new_key == '' or parse.new_key:
                     self.generate_key()
                 exit(0)
+            keys = self.infokes(show=False, storage=True)
+            self.num_key = {'default': v for k, v in keys.items() if k == parse.key}['default'] if parse.key != 'secret.key' else parse.key
+            print(self.num_key)
             self.path = parse.path if parse.path else 'encrypt_data.txt'
             self.write = parse.message if parse.message else False
             self.read = 'default' if parse.read == '' else parse.read
@@ -97,6 +98,14 @@ class crypt_end_decrypt:
     def main(self):
         first_execution = True
         if first_execution:
+            try:
+                os.mkdir('decrypt')
+            except:
+                pass
+            try:
+                os.mkdir('keys')
+            except:
+                pass
             with open(os.path.basename(__file__), 'r') as file:
                 content = file.read()
                 
@@ -110,7 +119,7 @@ class crypt_end_decrypt:
             self.decrypt_msg()
          
     def reading_secret(self):
-        with open('keys/secret.key', 'rb') as file:
+        with open(f'keys/{self.num_key}', 'rb') as file:
             secret = Fernet(file.read())
         return secret
 
@@ -135,11 +144,17 @@ class crypt_end_decrypt:
         else:
             print(decrypt_data.decode())
     
-    def infokes(self):
+    def infokes(self, show=True, storage=False):
+        key_info = dict()
         stdout = subprocess.check_output(['ls', 'keys']).decode().replace('\n', ', ').split(', ')
         for x in range(0, len(stdout)):
             if stdout[x]:
-                print(f'[{x}] {stdout[x]}')
+                if show:
+                    print(f'[{x}] {stdout[x]}')
+                if storage:
+                    key_info[str(x)] = stdout[x]
+        return key_info
+
 
 if __name__ == '__main__':
     start = crypt_end_decrypt()
